00afa3e7c4233e5bbc116e3b53eb1963291e7cac
joel-2004 <jrjoel484@gmail.com>
2023-10-05 06:19:44 +0530

    Bootstrap

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,13 +1,9 @@
-import Nav from "./Nav";
-import { BrowserRouter } from "react-router-dom";
+
 import "./App.css"
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Nav></Nav>
-        <h1>hello World</h1>
-      </BrowserRouter>
+      <h1>Hello</h1>
     </div>
   );
 }
