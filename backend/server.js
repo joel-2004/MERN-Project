e7f9500068d70471c0cfbcad0430700d50068f6e
joel-2004 <jrjoel484@gmail.com>
2024-01-19 14:16:42 +0530

    PORT env

--- backend/server.js
+++ backend/server.js
@@ -6,63 +6,7 @@ const toDoCollection = collection.toDoCollection;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
-app.get("/", cors(), (req, res) => {
-    res.end("Home");
-})
-
-app.post("/signup", async (req, res) => {
-    try {
-        //console.log("inside server.js");
-        const form = req.body.form;
-        const data = {
-            name: form.name,
-            password: form.password,
-            password1: form.password2
-        };
-        //console.log(data);
-        const checkUser = await userCollection.findOne({ name: data.name });//to check if the name is present
-        //console.log(checkUser);
-        if (checkUser) {
-            res.json("exist");
-        }
-        else {
-            // console.log("Added in db");
-            res.json("notexist");
-            await userCollection.insertMany([data]);
-        }
-    } catch (error) {
-        console.log(error);
-
-    }
-})
-
-app.post("/login", async (req, res) => {
-    const form = req.body.form;
-    const data = {
-        name: form.name,
-        password: form.password
-    }
-    //console.log(form.name);
-    console.log(data);
-    try {
-        const findUser = await userCollection.findOne({ name: data.name });
-
-        console.log(findUser);
-        if (findUser) {
-            if (findUser.password === data.password) {
-                res.json("Login");
-            }
-            else
-                res.json("Incorrectpassword");
-        }
-        else {
-            res.json("nouser");
-        }
-    } catch (e) {
-        console.log(e);
-    }
-})
+require("dotenv").config();
 
 app.get("/todo", async (req, res) => {
     try {
@@ -95,8 +39,6 @@ app.delete("/todo/delete/:id", async (req, res) => {
         console.log(error);
     }
 })
-
-
 
 
 app.put("/todo/update/:id", async (req, res) => {
